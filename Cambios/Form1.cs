1bf7821ad5526470069217ebe21924e811207891
luissalgueiro1985 <luisandresalgueiro@gmail.com>
2021-05-19 01:52:16 +0100

    metodo LoadRates

--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using Cambios.Modelos;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Windows.Forms;
 
 namespace Cambios
@@ -15,7 +12,30 @@ namespace Cambios
         public Form1()
         {
             InitializeComponent();
+            LoadRates();
         }
 
+        private async void LoadRates()
+        {
+            //bool load;
+
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://cambios.somee.com");
+
+            var response = await client.GetAsync("/api/rates");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase);
+                return;
+            }
+
+            var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+
+            ComboBoxOrigem.DataSource = rates;
+        }
     }
 }
